0d7539ab7f97f6f5ac280e38de7783e546d2cd9f
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2025-01-10 14:07:07 -0300

    up

--- App.js
+++ App.js
@@ -9,11 +9,20 @@ import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import VerifyEmailScreen from './screens/VerifyEmailScreen';
 import CadastroScreen from './screens/CadastroScreen';
 
+// Definindo o tipo para o Stack Navigator
+type RootStackParamList = {
+  Home: undefined;
+  SecondScreen: undefined;
+  Welcome: undefined;
+  Cadastro: undefined;
+  Login: undefined;
+  ForgotPassword: undefined;
+  VerifyEmail: undefined;
+};
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -21,11 +30,12 @@ export default function App() {
         <Stack.Screen name="SecondScreen" component={SecondScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
-
-<Stack.Screen name="Login" component={LoginScreen} />
-<Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-<Stack.Screen name="VerifyEmail" component={VerifyEmailScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="VerifyEmail" component={VerifyEmailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
